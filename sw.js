68621740b632d1c6e03ab3dc1b06dda3025c32c6
lommenz <49167116+lommenz@users.noreply.github.com>
2026-08-14 16:46:30 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const C="workout-log-v16";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json","./sw.js","./icon-180.png","./icon-192.png","./icon-512.png"]))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{let c=r.clone();caches.open(C).then(k=>k.put(e.request,c));return r}).catch(()=>caches.match("./index.html"))))});
+const C="workout-log-v16";
+const CORE=["./","./index.html","./manifest.json","./icon-180.png","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)))});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener("fetch",e=>{
+ if(e.request.method!=="GET")return;
+ if(e.request.mode==="navigate"){
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put("./index.html",copy));return r}).catch(()=>caches.match("./index.html")));
+  return;
+ }
+ e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r})));
+});
